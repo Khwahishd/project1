7e976eea2563054f065faa2c2de353d740268977
Khwahish Daudani <73955328+Khwahishd@users.noreply.github.com>
2023-09-24 21:28:03 -0700

    Add files via upload

---
+++ user_code.py
@@ -0,0 +1,11 @@
+
+    input_time_hours = int(input("Enter current hour: "))
+    input_alarm_hours = int(input("Enter alarm hours: "))
+    
+    total_alarm_hours = input_time_hours + input_alarm_hours
+    
+    alarm_days = total_alarm_hours // 24
+    alarm_hours = total_alarm_hours % 24
+    
+    print(f"The alarm will sound in {alarm_days} day(s) at {alarm_hours} hours")
+    
